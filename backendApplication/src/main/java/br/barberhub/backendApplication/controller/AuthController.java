ca6afd3c71c44cebd8e90390eba284c19dcd0979
Laura <laurabarros2541@gmail.com>
2025-11-01 03:42:20 -0300

    ajustes codigo

--- backendApplication/src/main/java/br/barberhub/backendApplication/controller/AuthController.java
+++ backendApplication/src/main/java/br/barberhub/backendApplication/controller/AuthController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("api/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
